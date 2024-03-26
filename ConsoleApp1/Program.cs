5397cd79fffd84470d71982c0e5e17893a7f0bdd
saq1rette <ruslan.2017012@gmail.com>
2024-03-26 20:59:47 +0300

    сортировка массива

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,2 @@
-﻿Console.WriteLine("Hello, World!");
-//
+﻿using ConsoleApp1;
+Programm1.main();
---
+++ ConsoleApp1/Programm1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    static class Programm1
+    {
+        public static void main()
+        {
+            //string w = ("Hello world".Print() + "!!!".Print()).Print();
+            int[] arr = new int[3].Select(x=>new Random().Next(100)).ToArray().Print();
+            //int[] arr1 = new int[15];
+            int[] arr1 = arr.OrderBy(x=>x).ToArray();
+            int[] arr2 = arr.OrderByDescending(x=>x).ToArray();
+            if (arr.SequenceEqual(arr1))
+            {
+                "OrderBy".Print();
+            }
+            else if (arr.SequenceEqual(arr2))
+            {
+                "OrderByDescending".Print();
+            }
+            else
+            {
+                "Random".Print();
+            }
+        }
+    }
+}
